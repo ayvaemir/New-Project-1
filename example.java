d4e79ca8f91f1688c7f85ffef2313e83828722bc
ayvaemir <emirayva@gmail.com>
2024-11-06 19:51:10 -0500

    secont commit

--- example.java
+++ example.java
@@ -3,5 +3,9 @@ package org.example.git;
 public class example {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for(int i=0; i<=100; i++){
+            System.out.println(i);
+        }
     }
 }
